ed9510256cc0737a939307dcc10bd1a58828affc
Anatoliy <itollik@yandex.ru>
2022-04-22 20:17:46 +0300

    fix

--- scripts/edit__profile.js
+++ scripts/edit__profile.js
@@ -3,33 +3,28 @@ const editProfile=document.querySelector('.profile__button-edit');//кнопка
 const profileName=document.querySelector('.profile__name');//поле имени
 const profileJob=document.querySelector('.profile__job');//поле профессии
 const popupClose=popupElement.querySelector('.popup__btn-close');//кнопка редактировать
-const popupSave=popupElement.querySelector('.popup__btn-save');// кнопка сохранить
 
 let nameInput=document.querySelector('#name');//input имя
 let jobInput=document.querySelector('#job');//input профессия
+let formElement=document.querySelector('.popup__form');//форма попапа
 
 function openPopup(){
-  popupElement.classList.remove('popup_opened');
-}
-function closePopup(){
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
   popupElement.classList.add('popup_opened');
 }
 
-editProfile.addEventListener('click', openPopup);
-popupClose.addEventListener('click',closePopup);
-
-
-let formElement=document.querySelector('.popup__form');
-nameInput.value = profileName.textContent;
-jobInput.value = profileJob.textContent;
+function closePopup(){
+  popupElement.classList.remove('popup_opened');
+}
 
 function formSubmitHandler (evt){
   evt.preventDefault();
-
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-
   closePopup();
 }
 
 formElement.addEventListener('submit',formSubmitHandler);
+editProfile.addEventListener('click', openPopup);
+popupClose.addEventListener('click',closePopup);
--- scripts/like.js
+++ scripts/like.js
@@ -1,9 +1,9 @@
-const likeButton = document.querySelector('.card__btn-like');
-
-
-function likeActive(){
-  likeButton.classList.add('card__btn-like_active');
-  likeButton.classList.remove('card__btn-like');
-}
-
-likeButton.addEventListener('click', likeActive);
+// const likeButton = document.querySelector('.card__btn-like');
+//
+//
+// function likeActive(){
+//   likeButton.classList.add('card__btn-like_active');
+//
+// }
+//
+// likeButton.addEventListener('click', likeActive);
